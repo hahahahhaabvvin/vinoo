4fa3d02f053498a2b8845134e466cd0ff1d664cc
hahahahhaabvvin <141469514+hahahahhaabvvin@users.noreply.github.com>
2023-08-26 11:01:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            }
-word = input("Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!): ")
+import discord
+import random
+from discord.ext import commands
+from bot_logic import gen_pass
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("tidak ada")
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+guess_game = None
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def pwd(ctx):
+    await ctx.send(gen_pass())
+
+@bot.command()
+async def guess(ctx):
+    global guess_game
+
+    guess_game = random.randint(1, 10)
+    
+    await ctx.send("Tebak angka 1 sampai 10, jawab dengan $answer")
+
+@bot.command()
+async def answer(ctx, number):
+    if guess_game == int(number):
+        await ctx.send("Selamat anda benar")
+    else:
+        await ctx.send("Jawaban salah, silahkan coba lagi")
+
+
+bot.run("TOKEN")
