c71a8284ec7eb82e08daae5ad1bd339a315a0255
hahahahhaabvvin <141469514+hahahahhaabvvin@users.noreply.github.com>
2023-09-02 11:20:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 import random
+import os
+import requests
 from discord.ext import commands
-from bot_logic import gen_pass
+from m1l2.bot_logic import gen_pass
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -40,5 +42,41 @@ async def answer(ctx, number):
     else:
         await ctx.send("Jawaban salah, silahkan coba lagi")
 
+@bot.command()
+async def mem(ctx):
+    daftar_gambar = os.listdir('images')
+    gambar = random.choice(daftar_gambar)
+    
+    with open(f'images/{gambar}', 'rb') as f:
+        # Mari simpan file perpustakaan/library Discord yang dikonversi dalam variabel ini!
+        picture = discord.File(f)
+    
+    # Kita kemudian dapat mengirim file ini sebagai tolok ukur!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+def get_dog_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+@bot.command('dog')
+async def dog(ctx):
+    '''Setelah kita memanggil perintah bebek (duck), program akan memanggil fungsi get_duck_image_url'''
+    image_url = get_dog_image_url()
+    await ctx.send(image_url)
 
 bot.run("TOKEN")
